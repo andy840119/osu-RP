f7bdb71ed7325d4e992b95445c0f1fc0bca0d9e9
andy840119@gmail.com <andy840119@gmail.com>
2017-11-01 00:38:00 +0900

    commit

--- osu.Game.Rulesets.RP/Judgements/RpJudgement.cs
+++ osu.Game.Rulesets.RP/Judgements/RpJudgement.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
-using System.Collections.Generic;
 using osu.Game.Rulesets.Judgements;
 using osu.Game.Rulesets.RP.Objects.Drawables.Play;
 using OpenTK;
@@ -11,13 +10,8 @@ namespace osu.Game.Rulesets.RP.Judgements
     public class RpJudgement : Judgement
     {
         public DrawableBaseRpObject RpObject;
-        //public RpScoreResult MaxScore;
 
-        // public RpScoreResult Score;
         public RpComboResult Combo;
-
-        //TODO : will be remove
-        public List<Vector2> HitExplosionPosition = new List<Vector2>();
 
         public Vector2 Position = new Vector2();
         public int AdditionalPlusScore = 0;
